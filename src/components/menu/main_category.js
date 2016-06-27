91093845460ee6c9bd1f95f27a19904cf2992624
Raúl <rdelgado@visual-engin.com>
2016-06-27 17:22:30 +0200

    actualización package.json para estilos scss

--- src/components/menu/main_category.js
+++ src/components/menu/main_category.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
-
 import SimpleCategory from './simple_category';
+
+import "./menu.scss";
 
 export default class MainCategory extends Component {
 
 	constructor(props){
 		super(props);
-		// console.log(this.props.category);
-
 	}
 
 
 	displayList(){
-
-				let cat =this.props.category;
-				let array = [];
-				console.log("hi");
-				console.log(cat);
-
-				for (var i = 0; i < cat.length; i++) {
-					array.push(<SimpleCategory category={cat[i]} />);
-				}
-				return array;
-			}
+		let cat =this.props.category;
+		let array = [];
+		for (var i = 0; i < cat.length; i++) {
+			array.push(<SimpleCategory key={cat[i].name} category={cat[i]} />);
+		}
+		return array;
+	}
 
 
 	render() {
 		return (
-
-			<div className="container">
-        <div>
-            <ul>
-								{this.displayList()}
-            </ul>
-        </div>
+			<div className="menu-container">
+				<div>
+					<ul>
+						{this.displayList()}
+					</ul>
+				</div>
 			</div>
 		);
 	}
